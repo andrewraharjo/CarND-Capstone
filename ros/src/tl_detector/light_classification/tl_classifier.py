977ba34cea6f7deac301b28a606dced3f391629c
Andrew Tsai <andrewraharjo@gmail.com>
2017-09-17 23:43:12 -0700

    beta release for classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,17 +1,69 @@
 from styx_msgs.msg import TrafficLight
 import numpy as np
+import rospkg
 import cv2
 from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers import Input, Dense
 import tensorflow as tf
 import json
 import rospy
 
 
-class TLClassifierCV(object):
+class TLClassifier(object):
     def __init__(self):
         # Lower and Upper threshold for color extraction
+        self.model = None
+        self.create_model()
+        self.load_ssd_model()
+        if not self.model:
+            rospy.logerr("Failed to traffic light classifier model")
+
+        self.colors = [TrafficLight.RED,
+                       TrafficLight.YELLOW,
+                       TrafficLight.GREEN,
+                       TrafficLight.UNKNOWN]
+
+        self.num_pixels = 950
+
         self.lower = np.array([150, 100, 150])
         self.upper = np.array([180, 255, 255])
+
+        # Define red pixels in hsv color space
+        self.lower_red_1 = np.array([0, 70, 50], dtype="uint8")
+        self.upper_red_1 = np.array([10, 255, 255], dtype="uint8")
+
+        self.lower_red_2 = np.array([170, 70, 50], dtype="uint8")
+        self.upper_red_2 = np.array([180, 255, 255], dtype="uint8")
+
+    def create_model(self):
+        self.model =  Sequential()
+        self.model.add(Dense(200, activation='relu', input_shape=(30000,)))
+        self.model.add(Dense(3, activation='softmax'))
+
+        rospack = rospkg.RosPack()
+        path_v = rospack.get_path('styx')
+        model_file = path_v+ \
+               '/../tl_detector/light_classification/model/tl-model-sim.h5'
+        self.model.load_weights(model_file)
+        self.graph = tf.get_default_graph()
+
+    def load_ssd_model(self):
+        rospack = rospkg.RosPack()
+        path_v = rospack.get_path('styx')
+        PATH_TO_CKPT = path_v + \
+                     '/../tl_detector/light_classification/model/ssd_sim.pb'
+        PATH_TO_LABELS = path_v + \
+                     '/../tl_detector/light_classification/model/tl_label_map.pbtxt'
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+            self.sess = tf.Session(graph=self.detection_graph)
 
     def get_classification(self, image):
         """
@@ -24,76 +76,14 @@ class TLClassifierCV(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        state = TrafficLight.UNKNOWN
-
-        red_area, red_image = self.get_colored_area(image, self.lower, self.upper)
-
-        # Needs careful tuning for the number of red pixesls
-        red_pixels = 40
-
-        if red_area > red_pixels:
-            state = TrafficLight.RED
-
-        return state
-
-    def get_colored_area(self, image, lower, upper):
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        mask_image = cv2.inRange(hsv_image, lower, upper)
-        extracted_image = cv2.bitwise_and(image, image, mask=mask_image)
-        area = cv2.countNonZero(mask_image)
-
-        return area, extracted_image
-
-
-class TLClassifier(object):
-
-    def __init__(self, experiment_environment):
-
-        if experiment_environment == "simulator":
-
-            model_path = 'light_classification/model/model_sim.json'
-            weights_path = 'light_classification/model/weights_sim.hdf5'
-
-        elif experiment_environment == "site":
-
-            model_path = 'light_classification/model/model_site.json'
-            weights_path = 'light_classification/model/weights_site.hdf5'
-
-        else:
-
-            raise ValueError("Launch is neither styx.launch nor site.launch!!!")
-
-        with open(model_path, 'r') as f:
-            loaded_model_json = f.read()
-
-        self.model = model_from_json(loaded_model_json)
-        self.model.load_weights(weights_path)
-        self.graph = tf.get_default_graph()
-
-    def get_classification(self, image):
-        """
-        Determines the color of the traffic light in image
-
-        Args:
-        image (cv::Mat): image containing the traffic light
-
-        Returns:
-        int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-        """
-        processed_image = self.process_image(image)
+        image_new = image[100:700, 50:550]
+        #dim = (100, 26)
+        r = 100.0 / image_new.shape[1]
+        dim = (100, int(image_new.shape[0] * r))
+        resized = cv2.resize(image_new, dim)
+        image_data = np.array([resized.flatten().tolist()])
+        image_data /= 255
         with self.graph.as_default():
-            predicted_class_id = self.model.predict_classes(processed_image, batch_size=1, verbose=0)
-        states_map = {0: TrafficLight.RED, 1: TrafficLight.YELLOW, 2: TrafficLight.GREEN}
-        # Get code for predicted state, return unknown if couldn't classify
-        state = states_map.get(predicted_class_id[0], TrafficLight.UNKNOWN)
-
-        return state
-
-    def process_image(self, image):
-        desired_shape = (128, 128)
-        image = cv2.resize(image, desired_shape, cv2.INTER_LINEAR)
-        image = image.astype('float32') / 255
-        processed_image = image.reshape(1, *image.shape)
-
-        return processed_image
+             classes = self.model.predict(image_data, batch_size=1)
+             return self.colors[np.argmax(classes[0])]
+        return TrafficLight.UNKNOWN
